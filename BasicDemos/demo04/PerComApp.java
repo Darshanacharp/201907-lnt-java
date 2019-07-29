eb73f2dbd35746b24012ecb5ff990aa50165a486
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-07-29 14:45:57 +0530

    day#1-update#4

--- BasicDemos/demo04/PerComApp.java
+++ BasicDemos/demo04/PerComApp.java
@@ -1,10 +1,11 @@
 class PerComApp{
 	public static void main(String[]args){
-		int n=5;
-		int r=3;
+		int n=Input.readInt("n?");
+		int r=Input.readInt("r?");
 		int p=Permutation.calculate(n,r);
 		int c=Combination.calculate(n,r);
-		System.out.println(n+"P"+r+"="+p);
-		System.out.printf("%dC%d=%d\n",n,r,c);
+
+		PrintWriter.write("permutation is "+p);
+		PrintWriter.write("combination is "+c);
 	}
 }
